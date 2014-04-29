afa9caa45082b7871ffcf911a914ac9e2c6cfc38
Mike Adler <mike.adler@huffingtonpost.com>
2014-04-29 14:26:07 -0400

    export NickNameHelper

--- awsheet/__init__.py
+++ awsheet/__init__.py
@@ -2,6 +2,7 @@ from .core import *
 from .helpers.awshelper import AWSHelper
 from .helpers.cloudformationhelper import CloudFormationHelper
 from .helpers.cnamehelper import CNAMEHelper
+from .helpers.nicknamehelper import NickNameHelper
 from .helpers.gslbhelper import GSLBHelper
 from .helpers.instancehelper import InstanceHelper
 from .helpers.securitygrouphelper import SecurityGroupHelper
